65b17be7b981c424ca04d83defc7c83872fc5187
Dani <daniel.fernandez@in2.es>
2018-11-29 14:03:40 +0100

    Projecte base

---
+++ src/main/java/es/in2/dsdibaapi/DsDibaApiApplication.java
@@ -0,0 +1,12 @@
+package es.in2.dsdibaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDibaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDibaApiApplication.class, args);
+	}
+}
